5a1d1dd902bdec2eb6530f8cb2b6991cf2f77a8e
Sean Ervinson <ervinsonong@gmail.com>
2020-01-04 22:58:07 +0800

    Added const style values

---
+++ lib/src/values/styles.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+import 'colors.dart';
+
+TextStyle title = TextStyle(
+  fontSize: 28.0,
+  color: bluePrimary,
+  fontWeight: FontWeight.bold,
+);
+
+TextStyle subtitle = TextStyle(
+  fontSize: 16.0,
+  color: Colors.black,
+  fontWeight: FontWeight.bold,
+);
+
+TextStyle button = TextStyle(
+  fontSize: 24.0,
+  fontWeight: FontWeight.bold,
+  color: Colors.white,
+);
