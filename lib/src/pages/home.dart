b3fac56f8ac3dc11e29abff3cff21388188a3d7d
Sean Ervinson <ervinsonong@gmail.com>
2020-01-04 22:58:58 +0800

    Added the bottom part of the screen

--- lib/src/pages/home.dart
+++ lib/src/pages/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../values/values.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -8,60 +9,123 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    var screenSize =
+    final screenHeight =
         MediaQuery.of(context).size.height - MediaQuery.of(context).padding.top;
+    final screenWidth = MediaQuery.of(context).size.width;
+    final TextEditingController destinationInputController =
+        TextEditingController();
     return SafeArea(
       child: Scaffold(
+        resizeToAvoidBottomInset: false,
         backgroundColor: Colors.white,
         body: Column(
           children: <Widget>[
             Container(
-              height: screenSize / 2,
+              height: screenHeight / 2,
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.center,
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
                   Text(
                     "Hello! Sean",
-                    style: TextStyle(
-                      fontSize: 16.0,
-                      color: Colors.black,
-                      fontWeight: FontWeight.bold,
-                    ),
+                    style: Theme.of(context).textTheme.subtitle,
                   ),
                   Text(
-                    "Destination",
-                    style: TextStyle(
-                      fontSize: 28.0,
-                      color: Color.fromRGBO(32, 176, 212, 1),
-                      fontWeight: FontWeight.bold,
-                    ),
+                    Strings.destination,
+                    style: Theme.of(context).textTheme.title,
                   ),
-                  TextField(),
-                  SizedBox(
-                    height: 32.0,
+                  Container(
+                    margin: EdgeInsets.symmetric(vertical: 24.0),
+                    width: 240.0,
+                    child: TextField(
+                      maxLines: 1,
+                      minLines: 1,
+                      controller: destinationInputController,
+                      textAlign: TextAlign.center,
+                      toolbarOptions: ToolbarOptions(
+                          paste: true, copy: true, cut: true, selectAll: true),
+                      style: TextStyle(),
+                    ),
                   ),
                   FlatButton(
                     padding:
-                        EdgeInsets.symmetric(horizontal: 80.0, vertical: 14.0),
+                        EdgeInsets.symmetric(horizontal: 80.0, vertical: 12.0),
                     textColor: Colors.white,
                     child: Text(
-                      "Start",
-                      style: TextStyle(fontSize: 24.0),
+                      Strings.start,
+                      style: Theme.of(context).textTheme.button,
                     ),
                     color: Color.fromRGBO(0, 177, 79, 1),
                     shape: RoundedRectangleBorder(
                       borderRadius: BorderRadius.circular(32.0),
                     ),
-                    onPressed: () => {},
+                    onPressed: () => print(destinationInputController.text),
                   )
                 ],
               ),
             ),
-            // Container(
-            //   height: screenSize / 2,
-            //   color: Color.fromRGBO(r, g, b, opacity),
-            // ),
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(48.0),
+                  topRight: Radius.circular(48.0),
+                ),
+                color: Color.fromRGBO(240, 240, 240, 1),
+              ),
+              padding: EdgeInsets.symmetric(vertical: 24.0, horizontal: 48.0),
+              height: screenHeight / 2,
+              child: Column(
+                children: <Widget>[
+                  Container(
+                    margin: EdgeInsets.only(bottom: 16.0, left: 4.0),
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      Strings.activityHeader,
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 18.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  Flexible(
+                    flex: 2,
+                    child: ListView(
+                      children: <Widget>[
+                        Container(
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: <Widget>[
+                              Text(
+                                "Home",
+                                style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 16.0,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.end,
+                                children: <Widget>[
+                                  Text("10/02/2019"),
+                                  Text("10:00pm"),
+                                ],
+                              )
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  GestureDetector(
+                    child: Icon(Icons.keyboard_arrow_up),
+                    onVerticalDragUpdate: (DragUpdateDetails details) =>
+                        {print(details.globalPosition)},
+                  )
+                ],
+              ),
+            ),
           ],
         ),
       ),
