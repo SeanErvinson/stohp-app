5e6ed7f3e4b1a5440ae4495519c60197e8945a14
Sean Ervinson <ervinsonong@gmail.com>
2020-01-04 22:57:53 +0800

    Added const color values

---
+++ lib/src/values/colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const Color bluePrimary = Color.fromRGBO(32, 176, 212, 1);
+const Color greenPrimary = Color.fromRGBO(0, 177, 79, 1);
+const Color redPrimary = Color.fromRGBO(32, 176, 212, 1);
+const Color bgSecondary = Color.fromRGBO(240, 240, 240, 1);
