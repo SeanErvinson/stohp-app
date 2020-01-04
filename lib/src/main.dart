7928ee25a3786b14e2bc48c54553abd088a779f6
Sean Ervinson <ervinsonong@gmail.com>
2020-01-04 18:53:18 +0800

    Added preferred orientation

--- lib/src/main.dart
+++ lib/src/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:stohp/src/pages/home.dart';
 
 void main() => runApp(StohpApp());
@@ -6,12 +7,13 @@ void main() => runApp(StohpApp());
 class StohpApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Stohp',
       theme: ThemeData(
         primarySwatch: Colors.blue,
-      ), 
+      ),
       home: HomePage(),
     );
   }
