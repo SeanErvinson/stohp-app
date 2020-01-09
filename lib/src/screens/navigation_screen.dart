e1a8fab717b11a99b8169d7e37327ec66680950e
Sean Ervinson <ervinsonong@gmail.com>
2020-01-09 18:13:36 +0800

    Added lower half of the navigation

--- lib/src/screens/navigation_screen.dart
+++ lib/src/screens/navigation_screen.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:stohp/src/values/values.dart';
 
 class NavigationScreen extends StatelessWidget {
   @override
@@ -20,7 +21,7 @@ class NavigationScreen extends StatelessWidget {
           Column(
             children: <Widget>[
               Container(
-                height: _usabelScreenHeight * .7,
+                height: _usabelScreenHeight * .70,
                 child: GoogleMap(
                   mapType: MapType.hybrid,
                   initialCameraPosition: _kGooglePlex,
@@ -29,8 +30,72 @@ class NavigationScreen extends StatelessWidget {
                   },
                 ),
               ),
+              Flexible(
+                flex: 1,
+                child: Container(
+                  padding:
+                      EdgeInsets.symmetric(horizontal: 14.0, vertical: 8.0),
+                  color: Colors.white,
+                  child: Column(
+                    children: <Widget>[
+                      Text(
+                        "Technohub Building H",
+                        style: Theme.of(context).textTheme.subhead,
+                        overflow: TextOverflow.ellipsis,
+                        softWrap: true,
+                        maxLines: 2,
+                        textAlign: TextAlign.center,
+                      ),
+                      Flexible(
+                        flex: 1,
+                        child: Container(
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: <Widget>[
+                              StatusInfo(
+                                title: Strings.distanceRemaining,
+                                value: "144",
+                                unit: "km",
+                              ),
+                              StatusInfo(
+                                title: Strings.estimatedTimeOfArrival,
+                                value: "54",
+                                unit: "mins",
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      FlatButton(
+                        child: Text(
+                          Strings.cancel,
+                          style: TextStyle(color: redSecondary),
+                        ),
+                        onPressed: () {},
+                      ),
+                    ],
+                  ),
+                ),
+              )
             ],
-          )
+          ),
+          Positioned(
+            top: (_usabelScreenHeight * .70) - 80,
+            left: MediaQuery.of(context).size.width / 2 - 120,
+            child: FlatButton(
+              padding: EdgeInsets.symmetric(horizontal: 80.0, vertical: 12.0),
+              textColor: Colors.white,
+              child: Text(
+                Strings.stohp,
+                style: Theme.of(context).textTheme.button,
+              ),
+              color: redPrimary,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(32.0),
+              ),
+              onPressed: () {},
+            ),
+          ),
         ],
       ),
     );
@@ -40,5 +105,39 @@ class NavigationScreen extends StatelessWidget {
     Position position = await Geolocator()
         .getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
     return position;
+  }
+}
+
+class StatusInfo extends StatelessWidget {
+  final String title;
+  final String value;
+  final String unit;
+
+  const StatusInfo({this.title, this.value, this.unit});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: <Widget>[
+        Text(
+          title,
+          style: Theme.of(context).textTheme.subhead,
+        ),
+        RichText(
+          text: TextSpan(
+            text: value,
+            style: Theme.of(context).textTheme.display3,
+            children: [
+              TextSpan(
+                text: unit,
+                style: Theme.of(context).textTheme.subhead,
+              )
+            ],
+          ),
+        ),
+      ],
+    );
   }
 }
