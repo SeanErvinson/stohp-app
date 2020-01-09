1e885f8c2d4353f0d3f20b17667928b520d93a3c
Sean Ervinson <ervinsonong@gmail.com>
2020-01-09 18:12:58 +0800

    Replace into constant values and added values

--- lib/src/values/strings.dart
+++ lib/src/values/strings.dart
@@ -1,5 +1,9 @@
 class Strings {
-  static String destination = "Destination";
-  static String start = "Start";
-  static String activityHeader = "Your Activity";
+  static const String destination = "Destination";
+  static const String start = "Start";
+  static const String activityHeader = "Your Activity";
+  static const String stohp = "Stohp";
+  static const String distanceRemaining = "Distance left";
+  static const String estimatedTimeOfArrival = "ETA";
+  static const String cancel = "Cancel";
 }
