5579a8c39c396ae2cd2d1e6eed74c06426c296fd
Sean Ervinson <ervinsonong@gmail.com>
2020-01-09 20:47:39 +0800

    Implemented initial autocomplete

--- lib/src/screens/home_screen.dart
+++ lib/src/screens/home_screen.dart
@@ -1,4 +1,8 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_typeahead/flutter_typeahead.dart';
+import 'package:google_maps_webservice/places.dart';
 
 import 'package:qrscan/qrscan.dart' as scanner;
 import 'package:qr_flutter/qr_flutter.dart';
@@ -11,26 +15,30 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  String barcode = '';
-  final TextEditingController destinationInputController =
-      TextEditingController();
-  var topVisible = false;
-  var bottomVisible = true;
+  PlacesSearchResult selected;
+  final TextEditingController _searchTextController = TextEditingController();
+  List<PlacesSearchResult> suggestions_places = [];
 
-  void changeVisibility() {
-    setState(() {
-      topVisible = !topVisible;
-      bottomVisible = !bottomVisible;
-    });
+  Future<List<PlacesSearchResult>> searchPlace(String query) async {
+    final places =
+        new GoogleMapsPlaces(apiKey: "AIzaSyDjhvBKoej7NWdGR5AOXUNaJLpLr4it5AY");
+    PlacesSearchResponse response = await places.searchByText(query);
+    return response.results;
+  }
+
+  void prettyPrint(List<PlacesSearchResult> results) {
+    for (var i = 0; i < results.length; i++) {
+      print("${i + 1} - ${results[i].name} :: ${results[i].formattedAddress}");
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    final screenHeight =
+    final _usableScreenHeight =
         MediaQuery.of(context).size.height - MediaQuery.of(context).padding.top;
-    final screenWidth = MediaQuery.of(context).size.width;
     var initial = 0.0;
     var distance = 0.0;
+
     return SafeArea(
       child: GestureDetector(
         onPanStart: (DragStartDetails details) {
@@ -48,15 +56,8 @@ class _HomeScreenState extends State<HomeScreen> {
           backgroundColor: Colors.white,
           body: Column(
             children: <Widget>[
-              Visibility(
-                visible: topVisible,
-                child: Container(
-                  color: Colors.red,
-                  height: 300.0,
-                ),
-              ),
               Container(
-                height: screenHeight / 2,
+                height: _usableScreenHeight / 2,
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.center,
                   mainAxisAlignment: MainAxisAlignment.center,
@@ -72,39 +73,37 @@ class _HomeScreenState extends State<HomeScreen> {
                     Container(
                       margin: EdgeInsets.symmetric(vertical: 24.0),
                       width: 240.0,
-                      child: TextField(
-                        maxLines: 1,
-                        minLines: 1,
-                        controller: destinationInputController,
-                        textAlign: TextAlign.center,
-                        toolbarOptions: ToolbarOptions(
-                            paste: true,
-                            copy: true,
-                            cut: true,
-                            selectAll: true),
-                        style: TextStyle(),
+                      child: TypeAheadField<PlacesSearchResult>(
+                        suggestionsBoxDecoration: SuggestionsBoxDecoration(
+                            constraints: BoxConstraints(
+                          maxHeight: 150.0,
+                        )),
+                        hideOnEmpty: true,
+                        textFieldConfiguration: TextFieldConfiguration(
+                          controller: _searchTextController,
+                          autofocus: true,
+                          style: Theme.of(context).textTheme.caption,
+                          maxLines: 1,
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            hintText: Strings.location,
+                          ),
+                        ),
+                        suggestionsCallback: (pattern) async {
+                          return searchPlace(pattern);
+                        },
+                        itemBuilder: (context, suggestion) {
+                          return ListTile(
+                            title: Text(suggestion.name),
+                            subtitle: Text(suggestion.formattedAddress),
+                          );
+                        },
+                        onSuggestionSelected: (suggestion) {
+                          print(suggestion.name);
+                          _searchTextController.text = suggestion.name;
+                        },
                       ),
                     ),
-                    // QrImage(
-                    //   data: "1234567890",
-                    //   version: QrVersions.auto,
-                    //   size: 160.0,
-                    //   gapless: false,
-                    //   embeddedImage: AssetImage('assets/logo/logo.png'),
-                    //   embeddedImageStyle: QrEmbeddedImageStyle(
-                    //     size: Size(24, 24),
-                    //   ),
-                    //   errorStateBuilder: (cxt, err) {
-                    //     return Container(
-                    //       child: Center(
-                    //         child: Text(
-                    //           "Uh oh! Something went wrong...",
-                    //           textAlign: TextAlign.center,
-                    //         ),
-                    //       ),
-                    //     );
-                    //   },
-                    // ),
                     FlatButton(
                       padding: EdgeInsets.symmetric(
                           horizontal: 80.0, vertical: 12.0),
@@ -118,46 +117,38 @@ class _HomeScreenState extends State<HomeScreen> {
                         borderRadius: BorderRadius.circular(32.0),
                       ),
                       onPressed: () {
-                        changeVisibility();
-                        print(destinationInputController.text);
+                        Navigator.pushNamed(context, "navigation");
                       },
                     )
                   ],
                 ),
               ),
-              Visibility(
-                visible: bottomVisible,
-                child: GestureDetector(
-                  onVerticalDragUpdate: (DragUpdateDetails details) =>
-                      print(details.globalPosition),
-                  child: Container(
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(48.0),
-                        topRight: Radius.circular(48.0),
-                      ),
-                      color: Color.fromRGBO(240, 240, 240, 1),
-                    ),
-                    padding:
-                        EdgeInsets.symmetric(vertical: 24.0, horizontal: 48.0),
-                    height: screenHeight / 2,
-                    child: Column(
-                      children: <Widget>[
-                        Container(
-                          margin: EdgeInsets.only(bottom: 16.0, left: 4.0),
-                          alignment: Alignment.centerLeft,
-                          child: Text(
-                            Strings.activityHeader,
-                            style: TextStyle(
-                              color: Colors.black,
-                              fontSize: 18.0,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
+              // TODO: Change into Sliver
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(48.0),
+                    topRight: Radius.circular(48.0),
                   ),
+                  color: Color.fromRGBO(240, 240, 240, 1),
+                ),
+                padding: EdgeInsets.symmetric(vertical: 24.0, horizontal: 48.0),
+                height: _usableScreenHeight / 2,
+                child: Column(
+                  children: <Widget>[
+                    Container(
+                      margin: EdgeInsets.only(bottom: 16.0, left: 4.0),
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        Strings.activityHeader,
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 18.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ),
             ],
@@ -167,14 +158,13 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 
-  Future _scan() async {
-    String barcode = await scanner.scan();
-    setState(() => destinationInputController.text = barcode);
-  }
-
   @override
   void dispose() {
-    destinationInputController.dispose();
+    _searchTextController.dispose();
     super.dispose();
+  }
+
+  Future _scan() async {
+    String barcode = await scanner.scan();
   }
 }
