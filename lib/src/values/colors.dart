d53007b0435a65c64dc3b2165ab4ab2d37c39b9e
Sean Ervinson <ervinsonong@gmail.com>
2020-01-15 09:34:11 +0800

    Added colors for time mood

--- lib/src/values/colors.dart
+++ lib/src/values/colors.dart
@@ -2,6 +2,14 @@ import 'package:flutter/material.dart';
 
 const Color bluePrimary = Color.fromRGBO(32, 176, 212, 1);
 const Color greenPrimary = Color.fromRGBO(0, 177, 79, 1);
-const Color bgSecondary = Color.fromRGBO(240, 240, 240, 1);
+const Color bgSecondary = Color.fromRGBO(245, 245, 245, 1);
 const Color redPrimary = Color.fromRGBO(238, 98, 81, 1);
 const Color redSecondary = Color.fromRGBO(240, 66, 45, 1);
+
+
+const Color eveningColor = Color.fromRGBO(251, 164, 166, 1);
+const Color darkBluePurple = Color.fromRGBO(41, 30, 100, 1);
+const Color darkBlue = Color.fromRGBO(29, 112, 208, 1);
+const Color lightBlue = Color.fromRGBO(139, 183, 254, 1);
+const Color darkOrange = Color.fromRGBO(240, 66, 45, 1);
+const Color lightOrange = Color.fromRGBO(253, 204, 165, 1);
