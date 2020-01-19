6ce0547a1e75ec70bffd3cc089399123b2b72ee0
Sean Ervinson <ervinsonong@gmail.com>
2020-01-19 14:44:58 +0800

    Changed homescreen to stateless

--- lib/src/screens/home_screen.dart
+++ lib/src/screens/home_screen.dart
@@ -3,34 +3,15 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter_typeahead/flutter_typeahead.dart';
 import 'package:google_maps_webservice/places.dart';
-
 import 'package:qrscan/qrscan.dart' as scanner;
-import 'package:qr_flutter/qr_flutter.dart';
 
 import '../values/values.dart';
 
-class HomeScreen extends StatefulWidget {
-  @override
-  _HomeScreenState createState() => _HomeScreenState();
-}
+class HomeScreen extends StatelessWidget {
+  // Will be replaced with the user model
+  final String name;
 
-class _HomeScreenState extends State<HomeScreen> {
-  PlacesSearchResult selected;
-  final TextEditingController _searchTextController = TextEditingController();
-
-  Future<List<PlacesSearchResult>> searchPlace(String query) async {
-    final places =
-        new GoogleMapsPlaces(apiKey: "AIzaSyDjhvBKoej7NWdGR5AOXUNaJLpLr4it5AY");
-    PlacesSearchResponse response = await places.searchByText(query);
-    return response.results;
-  }
-
-  void prettyPrint(List<PlacesSearchResult> results) {
-    for (var i = 0; i < results.length; i++) {
-      print("${i + 1} - ${results[i].name} :: ${results[i].formattedAddress}");
-    }
-  }
-
+  const HomeScreen({Key key, this.name}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     final _usableScreenHeight =
@@ -53,109 +34,84 @@ class _HomeScreenState extends State<HomeScreen> {
         child: Scaffold(
           resizeToAvoidBottomInset: false,
           backgroundColor: Colors.white,
-          body: Column(
-            children: <Widget>[
-              Container(
-                height: _usableScreenHeight / 2,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    RichText(
-                      text: TextSpan(
-                        text: "Hello ",
-                        style: Theme.of(context).textTheme.subtitle,
-                        children: [
-                          TextSpan(
-                            text: "Sean",
-                            style: Theme.of(context).textTheme.subtitle,
-                          ),
-                        ],
+          body: CustomScrollView(
+            slivers: <Widget>[
+              SliverAppBar(
+                backgroundColor: Colors.white,
+                expandedHeight: _usableScreenHeight * .45,
+                flexibleSpace: FlexibleSpaceBar(
+                  background: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      GreetingHeader(),
+                      Text(
+                        Strings.destination,
+                        style: Theme.of(context).textTheme.title,
                       ),
-                    ),
-                    Text(
-                      Strings.destination,
-                      style: Theme.of(context).textTheme.title,
-                    ),
-                    Container(
-                      margin: EdgeInsets.symmetric(vertical: 24.0),
-                      width: 240.0,
-                      child: TypeAheadField<PlacesSearchResult>(
-                        suggestionsBoxDecoration: SuggestionsBoxDecoration(
-                            constraints: BoxConstraints(
-                          maxHeight: 150.0,
-                        )),
-                        hideOnEmpty: true,
-                        textFieldConfiguration: TextFieldConfiguration(
-                          controller: _searchTextController,
-                          autofocus: true,
-                          style: Theme.of(context).textTheme.caption,
-                          maxLines: 1,
-                          decoration: InputDecoration(
-                            border: OutlineInputBorder(),
-                            hintText: Strings.location,
-                          ),
+                      LocationAutoCompleteTextField(),
+                      FlatButton(
+                        padding: EdgeInsets.symmetric(
+                            horizontal: 80.0, vertical: 12.0),
+                        textColor: Colors.white,
+                        child: Text(
+                          Strings.start,
+                          style: Theme.of(context).textTheme.button,
                         ),
-                        suggestionsCallback: (pattern) async {
-                          return searchPlace(pattern);
+                        color: Color.fromRGBO(0, 177, 79, 1),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(32.0),
+                        ),
+                        onPressed: () {
+                          Navigator.pushNamed(context, "navigation");
                         },
-                        itemBuilder: (context, suggestion) {
-                          return ListTile(
-                            title: Text(suggestion.name),
-                            subtitle: Text(suggestion.formattedAddress),
-                          );
-                        },
-                        onSuggestionSelected: (suggestion) {
-                          print(suggestion.name);
-                          _searchTextController.text = suggestion.name;
-                        },
-                      ),
-                    ),
-                    FlatButton(
-                      padding: EdgeInsets.symmetric(
-                          horizontal: 80.0, vertical: 12.0),
-                      textColor: Colors.white,
-                      child: Text(
-                        Strings.start,
-                        style: Theme.of(context).textTheme.button,
-                      ),
-                      color: Color.fromRGBO(0, 177, 79, 1),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(32.0),
-                      ),
-                      onPressed: () {
-                        Navigator.pushNamed(context, "navigation");
-                      },
-                    )
-                  ],
+                      )
+                    ],
+                  ),
                 ),
               ),
-              // TODO: Change into Sliver
-              Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(48.0),
-                    topRight: Radius.circular(48.0),
-                  ),
-                  color: Color.fromRGBO(240, 240, 240, 1),
+              // TODO: Animation when first sliver is 0
+              // Add floating or change icon
+              // Change border to zero
+              SliverAppBar(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(32),
+                  topRight: Radius.circular(32),
+                )),
+                primary: true,
+                leading: Icon(
+                  Icons.directions_run,
+                  color: Colors.black,
                 ),
-                padding: EdgeInsets.symmetric(vertical: 24.0, horizontal: 48.0),
-                height: _usableScreenHeight / 2,
-                child: Column(
-                  children: <Widget>[
-                    Container(
-                      margin: EdgeInsets.only(bottom: 16.0, left: 4.0),
-                      alignment: Alignment.centerLeft,
-                      child: Text(
-                        Strings.activityHeader,
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 18.0,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ),
-                  ],
+                elevation: 0.0,
+                pinned: true,
+                backgroundColor: bgSecondary,
+                title: Text(
+                  Strings.activityHeader,
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 18.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              SliverList(
+                delegate: SliverChildBuilderDelegate(
+                  (BuildContext context, int index) {
+                    if (index.isOdd)
+                      return Container(
+                        child: Divider(),
+                        color: bgSecondary,
+                      );
+                    final i = index ~/ 2;
+                    return ActivityTile(
+                      place: i.toString(),
+                      time: "10:00pm,",
+                      date: "01/02/03",
+                    );
+                  },
+                  childCount: 25 * 2,
                 ),
               ),
             ],
@@ -164,14 +120,136 @@ class _HomeScreenState extends State<HomeScreen> {
       ),
     );
   }
+}
 
+Future _scan() async {
+  String barcode = await scanner.scan();
+}
+
+class GreetingHeader extends StatelessWidget {
   @override
-  void dispose() {
-    _searchTextController.dispose();
-    super.dispose();
+  Widget build(BuildContext context) {
+    return RichText(
+      text: TextSpan(
+        text: "Hello ",
+        style: Theme.of(context).textTheme.subtitle,
+        children: [
+          TextSpan(
+            text: "Sean",
+            style: Theme.of(context).textTheme.subtitle,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class LocationAutoCompleteTextField extends StatelessWidget {
+  final TextEditingController _searchTextController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 24.0),
+      width: 300.0,
+      child: TypeAheadField<PlacesSearchResult>(
+        suggestionsBoxDecoration: SuggestionsBoxDecoration(
+            constraints: BoxConstraints(
+          maxHeight: 150.0,
+        )),
+        hideOnEmpty: true,
+        textFieldConfiguration: TextFieldConfiguration(
+          controller: _searchTextController,
+          style: Theme.of(context).textTheme.caption,
+          maxLines: 1,
+          decoration: InputDecoration(
+            border: OutlineInputBorder(),
+            hintText: Strings.location,
+          ),
+        ),
+        suggestionsCallback: (pattern) async {
+          return searchPlace(pattern);
+        },
+        itemBuilder: (context, suggestion) {
+          return SuggestionTile(
+            title: suggestion.name,
+            subtitle: suggestion.formattedAddress,
+          );
+        },
+        onSuggestionSelected: (suggestion) {
+          print(suggestion.name);
+          _searchTextController.text = suggestion.name;
+        },
+      ),
+    );
   }
 
-  Future _scan() async {
-    String barcode = await scanner.scan();
+  Future<List<PlacesSearchResult>> searchPlace(String query) async {
+    final places =
+        new GoogleMapsPlaces(apiKey: "AIzaSyDjhvBKoej7NWdGR5AOXUNaJLpLr4it5AY");
+    PlacesSearchResponse response = await places.searchByText(query);
+    return response.results;
+  }
+}
+
+class ActivityTile extends StatelessWidget {
+  final String place;
+  final String date;
+  final String time;
+
+  const ActivityTile({this.place, this.date, this.time});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 24, vertical: 14),
+      color: bgSecondary,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Text(
+            place,
+            style: Theme.of(context).textTheme.subtitle,
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(date),
+              Text(time),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class SuggestionTile extends StatelessWidget {
+  final String title;
+  final String subtitle;
+
+  const SuggestionTile({this.title, this.subtitle});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      padding: EdgeInsets.symmetric(horizontal: 4, vertical: 8),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(
+            title,
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+          Text(
+            subtitle,
+            style: TextStyle(fontSize: 10.0),
+            maxLines: 1,
+            overflow: TextOverflow.ellipsis,
+          ),
+          Divider(),
+        ],
+      ),
+    );
   }
 }
