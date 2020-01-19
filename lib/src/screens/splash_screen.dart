88c2bc8f102d65365613e268952dc26a1b149260
Sean Ervinson <ervinsonong@gmail.com>
2020-01-19 23:07:17 +0800

    Tweaked splash screen

--- lib/src/screens/splash_screen.dart
+++ lib/src/screens/splash_screen.dart
@@ -1,12 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:stohp/src/values/values.dart';
 
 class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final double _usableScreenHeight = MediaQuery.of(context).size.height;
     return SafeArea(
       child: Scaffold(
-        body: Center(
-          child: Text("Stohp"),
+        body: Column(
+          children: <Widget>[
+            Flexible(
+              flex: 1,
+              child: Container(
+                color: Colors.white,
+                child: Center(
+                  child: Image.asset(
+                    "assets/icons/logo-banner.png",
+                    width: 180.0,
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              height: _usableScreenHeight * .05,
+              decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                      colors: [logoSecondary, logoPrimary])),
+            ),
+          ],
         ),
       ),
     );
