5cc9858380f05d8b1926a369d59253b3cc69eb45
Sean Ervinson <ervinsonong@gmail.com>
2020-01-27 17:21:49 +0800

    Revamped google button

--- lib/src/components/login/login_google_button.dart
+++ lib/src/components/login/login_google_button.dart
@@ -1,22 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:stohp/src/components/login/bloc/bloc.dart';
+import 'package:stohp/src/values/values.dart';
 
 class GoogleLoginButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return RaisedButton.icon(
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(30.0),
+    return Container(
+      width: double.infinity,
+      child: FlatButton.icon(
+        icon: Image.asset(
+          "assets/icons/google-logo.png",
+          height: 32.0,
+        ),
+        onPressed: () {
+          BlocProvider.of<LoginBloc>(context).add(
+            LoginWithGooglePressed(),
+          );
+        },
+        label: Text(Strings.googleLogin,
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 10.0,
+            )),
+        color: Color.fromRGBO(66, 133, 244, 1),
       ),
-      icon: Icon(Icons.account_balance),
-      onPressed: () {
-        BlocProvider.of<LoginBloc>(context).add(
-          LoginWithGooglePressed(),
-        );
-      },
-      label: Text('Sign in with Google', style: TextStyle(color: Colors.white)),
-      color: Colors.redAccent,
     );
   }
 }
