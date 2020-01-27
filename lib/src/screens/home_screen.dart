682f80214815fb2c3d99de799dd64640f796418a
Sean Ervinson <ervinsonong@gmail.com>
2020-01-28 00:09:17 +0800

    Separated article to its own

--- lib/src/screens/home_screen.dart
+++ lib/src/screens/home_screen.dart
@@ -164,38 +164,51 @@ class NewsStoriesCard extends StatelessWidget {
                             snapshot.data != null ? snapshot.data.length : 0,
                         itemBuilder: (BuildContext context, int index) {
                           Article currentArticle = snapshot.data[index];
-                          return Container(
-                            width: 148.0,
-                            child: Card(
-                              clipBehavior: Clip.antiAliasWithSaveLayer,
-                              color: bgSecondary,
-                              child: Column(
-                                children: <Widget>[
-                                  AspectRatio(
-                                    aspectRatio: 16 / 9,
-                                    child: FittedBox(
-                                      fit: BoxFit.cover,
-                                      child:
-                                          Image.network(currentArticle.image),
-                                    ),
-                                  ),
-                                  Padding(
-                                    padding: EdgeInsets.all(6.0),
-                                    child: Text(
-                                      currentArticle.title,
-                                      style: TextStyle(fontSize: 12),
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ),
-                          );
+                          return ArticleTile(currentArticle: currentArticle);
                         },
                       );
                   }
                 },
               ),
             )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ArticleTile extends StatelessWidget {
+  const ArticleTile({
+    Key key,
+    @required this.currentArticle,
+  }) : super(key: key);
+
+  final Article currentArticle;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 148.0,
+      child: Card(
+        clipBehavior: Clip.antiAliasWithSaveLayer,
+        color: bgSecondary,
+        child: Column(
+          children: <Widget>[
+            AspectRatio(
+              aspectRatio: 16 / 9,
+              child: FittedBox(
+                fit: BoxFit.cover,
+                child: Image.network(currentArticle.image),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(6.0),
+              child: Text(
+                currentArticle.title,
+                style: TextStyle(fontSize: 12),
+              ),
+            ),
           ],
         ),
       ),
