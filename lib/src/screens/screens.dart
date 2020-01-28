48b13af4eb32da2329ed71f2cf689bfc3d0ebbb5
Sean Ervinson <ervinsonong@gmail.com>
2020-01-28 16:39:53 +0800

    Removed navigation screen

--- lib/src/screens/screens.dart
+++ lib/src/screens/screens.dart
@@ -2,6 +2,5 @@ export 'welcome_screen.dart';
 export 'registration_screen.dart';
 export 'settings_screen.dart';
 export 'home_screen.dart';
-export 'navigation_screen.dart';
 export 'splash_screen.dart';
 export 'login_screen.dart';
