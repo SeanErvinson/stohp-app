3a66ddca67e1596b90a83f3a9ea6fc703d14768a
Sean Ervinson <ervinsonong@gmail.com>
2020-02-03 00:03:46 +0800

    Added logic for wake and scan

--- lib/src/components/home/services/services_section.dart
+++ lib/src/components/home/services/services_section.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
 import 'package:stohp/src/components/common/card_header.dart';
+import 'package:stohp/src/components/home/services/bloc/location_track_bloc.dart';
 import 'package:stohp/src/values/values.dart';
+
+import 'bloc/stop_bloc.dart';
 
 class ServicesSection extends StatelessWidget {
   const ServicesSection({
@@ -25,15 +30,56 @@ class ServicesSection extends StatelessWidget {
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: <Widget>[
-                  ServiceButton(
-                    title: Strings.wakeService,
-                    icon: Icons.alarm,
-                    onPressed: () {},
+                  BlocBuilder<LocationTrackBloc, LocationTrackState>(
+                    builder: (context, state) {
+                      VoidCallback onClick;
+                      if (state is LocationRunning)
+                        onClick = () => _onConfirmationDialog(context);
+                      else
+                        onClick = () => Navigator.of(context)
+                            .pushNamed("location-destination");
+                      return ServiceButton(
+                        title: Strings.wakeService,
+                        icon: Icons.alarm,
+                        onPressed: onClick,
+                      );
+                    },
                   ),
-                  ServiceButton(
-                    title: Strings.stopService,
-                    icon: Icons.pause,
-                    onPressed: () {},
+                  BlocListener<StopBloc, StopState>(
+                    listener: (context, state) {
+                      if (state is StopScanFailed) {
+                        Scaffold.of(context)
+                          ..hideCurrentSnackBar()
+                          ..showSnackBar(
+                            SnackBar(
+                              content: Container(
+                                height:
+                                    MediaQuery.of(context).size.height * .02,
+                                child: Text(
+                                  Strings.scanError,
+                                  style: TextStyle(
+                                    fontSize: 12,
+                                  ),
+                                ),
+                              ),
+                              backgroundColor: Colors.red,
+                            ),
+                          );
+                      }
+                    },
+                    child: BlocBuilder<StopBloc, StopState>(
+                      builder: (context, state) {
+                        if (state is StopQRCaptured) {
+                          return StopButton(qrCode: state.qrCode);
+                        }
+                        return ServiceButton(
+                          title: Strings.scanService,
+                          icon: Icons.pause,
+                          onPressed: () => BlocProvider.of<StopBloc>(context)
+                              .add(ScanQREvent()),
+                        );
+                      },
+                    ),
                   ),
                   ServiceButton(
                     title: Strings.mapService,
@@ -48,6 +94,60 @@ class ServicesSection extends StatelessWidget {
       ),
     );
   }
+
+  _onConfirmationDialog(BuildContext context) {
+    Alert(
+      context: context,
+      type: AlertType.warning,
+      title: Strings.cancelTripTitle,
+      desc: Strings.cancelTripDesc,
+      buttons: [
+        DialogButton(
+            child: Text(
+              Strings.yesOption,
+              style: TextStyle(color: Colors.white, fontSize: 20),
+            ),
+            onPressed: () {
+              // Cancel current trip
+              Navigator.of(context).pop();
+              Navigator.of(context).pushNamed("location-destination");
+            }),
+        DialogButton(
+          child: Text(
+            Strings.noOption,
+            style: TextStyle(color: Colors.white, fontSize: 20),
+          ),
+          onPressed: () => Navigator.pop(context),
+        )
+      ],
+    ).show();
+  }
+}
+
+class StopButton extends StatelessWidget {
+  final String _qrCode;
+  const StopButton({Key key, String qrCode})
+      : this._qrCode = qrCode,
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final StopBloc _bloc = BlocProvider.of<StopBloc>(context);
+    return Column(
+      children: <Widget>[
+        GestureDetector(
+          onTap: () => _bloc.add(SendStopRequest(_qrCode)),
+          onLongPress: () => _bloc.add(CancelStop()),
+          child: CircleAvatar(
+            backgroundColor: redPrimary,
+            foregroundColor: Colors.white,
+            child: const Icon(Icons.stop, size: 24.0),
+          ),
+        ),
+        Text(Strings.stopService)
+      ],
+    );
+  }
 }
 
 class ServiceButton extends StatelessWidget {
@@ -55,9 +155,12 @@ class ServiceButton extends StatelessWidget {
   final VoidCallback _onPressed;
   final IconData _icon;
 
-  const ServiceButton(
-      {Key key, String title, VoidCallback onPressed, IconData icon})
-      : this._title = title,
+  const ServiceButton({
+    Key key,
+    String title,
+    VoidCallback onPressed,
+    IconData icon,
+  })  : this._title = title,
         this._onPressed = onPressed,
         this._icon = icon,
         super(key: key);
@@ -67,6 +170,8 @@ class ServiceButton extends StatelessWidget {
     return Column(
       children: <Widget>[
         CircleAvatar(
+          backgroundColor: darkBlue,
+          foregroundColor: Colors.white,
           child: IconButton(
             icon: Icon(_icon),
             iconSize: 24.0,
