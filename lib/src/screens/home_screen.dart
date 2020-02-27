00ea0199a89018d514228aa093d6d7c315b3863c
Sean Ervinson <ervinsonong@gmail.com>
2020-02-27 15:51:37 +0800

    Replaced displayName with user

--- lib/src/screens/home_screen.dart
+++ lib/src/screens/home_screen.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:stohp/src/components/common/bloc/alert_bloc.dart';
 import 'package:stohp/src/components/home/news_stories/news_stories_section.dart';
 import 'package:stohp/src/components/home/services/bloc/wake_bloc.dart';
 import 'package:stohp/src/components/home/services/services_section.dart';
 import 'package:stohp/src/components/home/services/wake/travel_status_bar.dart';
+import 'package:stohp/src/models/user.dart';
 import 'package:stohp/src/values/values.dart';
 
 class HomeScreen extends StatelessWidget {
-  final String name;
-  const HomeScreen({Key key, this.name}) : super(key: key);
+  final User _user;
+  const HomeScreen({Key key, User user}) : _user = user, super(key: key);
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -41,6 +43,9 @@ class HomeScreen extends StatelessWidget {
               bloc: BlocProvider.of<WakeBloc>(context),
               builder: (context, state) {
                 if (state is WakeRunning) {
+                  if (state.distance.distanceInValue < 50) {
+                    BlocProvider.of<AlertBloc>(context).add(TriggerAlert());
+                  }
                   return Positioned(
                     bottom: 0,
                     child: TravelStatusBar(
