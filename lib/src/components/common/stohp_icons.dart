2090236543550ad213c194f2c1bc9aba01c110fb
Sean Ervinson <ervinsonong@gmail.com>
2020-02-27 18:48:38 +0800

    Added custom icons

---
+++ lib/src/components/common/stohp_icons.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/widgets.dart';
+
+class Stohp {
+  Stohp._();
+
+  static const _kFontFam = 'Stohp';
+
+  static const IconData scan = const IconData(0xe800, fontFamily: _kFontFam);
+  static const IconData qr = const IconData(0xe802, fontFamily: _kFontFam);
+  static const IconData block = const IconData(0xe803, fontFamily: _kFontFam);
+  static const IconData user = const IconData(0xe804, fontFamily: _kFontFam);
+  static const IconData map_signs = const IconData(0xf277, fontFamily: _kFontFam);
+}
