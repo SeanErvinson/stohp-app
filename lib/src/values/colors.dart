f2650f142c15bbafe6bce5f93eb474e0585a7e2b
Sean Ervinson <ervinsonong@gmail.com>
2020-03-01 20:40:02 +0800

    Changed afternoon colors

--- lib/src/values/colors.dart
+++ lib/src/values/colors.dart
@@ -11,5 +11,5 @@ const Color eveningColor = Color.fromRGBO(102, 109, 137, 1);
 const Color darkEveningPurple = Color.fromRGBO(50, 53, 70, 1);
 const Color darkBlue = Color.fromRGBO(57, 196, 253, 1);
 const Color lightBlue = Color.fromRGBO(189, 247, 252, 1);
-const Color darkOrange = Color.fromRGBO(205, 78, 123, 1);
-const Color lightOrange = Color.fromRGBO(255, 170, 115, 1);
+const Color darkOrange = Color.fromRGBO(248, 173, 56, 1);
+const Color lightOrange = Color.fromRGBO(252, 235, 33, 1);
