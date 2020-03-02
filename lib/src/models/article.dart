e64be77d964d3d83031634fd90e58c0c00c62802
Sean Ervinson <ervinsonong@gmail.com>
2020-03-02 18:10:59 +0800

    Changed initial article model

--- lib/src/models/article.dart
+++ lib/src/models/article.dart
@@ -1,24 +1,36 @@
 class Article {
-  String id;
+  int id;
   String title;
-  String content;
+  String slug;
   String image;
+  String content;
+  String createdOn;
 
-  Article({this.id, this.title, this.content, this.image});
+  Article(
+      {this.id,
+      this.title,
+      this.slug,
+      this.image,
+      this.content,
+      this.createdOn});
 
   Article.fromJson(Map<String, dynamic> json) {
     id = json['id'];
     title = json['title'];
-    content = json['content'];
+    slug = json['slug'];
     image = json['image'];
+    content = json['content'];
+    createdOn = json['created_on'];
   }
 
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = new Map<String, dynamic>();
     data['id'] = this.id;
     data['title'] = this.title;
-    data['content'] = this.content;
+    data['slug'] = this.slug;
     data['image'] = this.image;
+    data['content'] = this.content;
+    data['created_on'] = this.createdOn;
     return data;
   }
 }
