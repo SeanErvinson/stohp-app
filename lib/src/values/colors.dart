443af5305d3ca9be0fd3817c7828cf17cb9770a3
Sean Ervinson <ervinsonong@gmail.com>
2020-03-07 22:18:00 +0800

    Changed bg color to lighter

--- lib/src/values/colors.dart
+++ lib/src/values/colors.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 const Color bluePrimary = Color.fromRGBO(1, 83, 182, 1);
 const Color greenPrimary = Color.fromRGBO(0, 177, 79, 1);
-const Color bgPrimary = Color.fromRGBO(238, 238, 238, 1);
+const Color bgPrimary = Color.fromRGBO(245, 245, 245, 1);
 const Color redPrimary = Color.fromRGBO(238, 98, 81, 1);
 const Color colorPrimary = Color.fromRGBO(211, 61, 61, 1);
 const Color colorSecondary = Color.fromRGBO(238, 115, 115, 1);
