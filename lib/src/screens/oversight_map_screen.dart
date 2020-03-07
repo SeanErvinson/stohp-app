0c52c860ea15ef9554bf825c03e182c0adb98436
Sean Ervinson <ervinsonong@gmail.com>
2020-03-07 17:43:28 +0800

    Added initial ui for oversight

--- lib/src/screens/oversight_map_screen.dart
+++ lib/src/screens/oversight_map_screen.dart
@@ -1,10 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:stohp/src/components/home/services/view/oversight_map.dart';
+import 'package:stohp/src/values/values.dart';
 
 class OversightMapScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-
+    return SafeArea(
+      child: Scaffold(
+        body: Stack(
+          children: <Widget>[
+            OversightMap(),
+            Positioned(
+              top: 10,
+              left: 10,
+              child: FloatingActionButton.extended(
+                onPressed: () => Navigator.of(context).pop(),
+                foregroundColor: colorSecondary,
+                backgroundColor: Colors.white,
+                icon: Icon(
+                  Icons.arrow_back,
+                  size: 16.0,
+                ),
+                label: Text(
+                  Strings.back,
+                  style: primaryAppText.copyWith(fontSize: 12.0),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
