6d87a18f7deca0445df5d28696c1e38a2803294d
Sean Ervinson <ervinsonong@gmail.com>
2020-03-09 14:53:41 +0800

    Added filter box

--- lib/src/screens/oversight_map_screen.dart
+++ lib/src/screens/oversight_map_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:stohp/src/components/home/services/view/bloc/oversight_bloc.dart';
+import 'package:stohp/src/components/home/services/view/filter_form.dart';
 import 'package:stohp/src/components/home/services/view/oversight_map.dart';
 import 'package:stohp/src/components/profile/profile_screen_argument.dart';
 import 'package:stohp/src/models/commuter_oversight_info.dart';
@@ -9,6 +10,9 @@ import 'package:stohp/src/values/values.dart';
 class OversightMapScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final _usableScreenHeight =
+        MediaQuery.of(context).size.height - MediaQuery.of(context).padding.top;
+    final _usableScreenWidth = MediaQuery.of(context).size.width;
     final UserArgument args = ModalRoute.of(context).settings.arguments;
     final User user = args.user;
     final OversightBloc _oversightBloc =
@@ -37,6 +41,20 @@ class OversightMapScreen extends StatelessWidget {
                   style: primaryAppText.copyWith(fontSize: 12.0),
                 ),
               ),
+            ),
+            Positioned(
+              width: _usableScreenWidth,
+              child: Container(
+                height: _usableScreenHeight * .3,
+                child: Card(
+                  color: Colors.white,
+                  margin: EdgeInsets.all(8.0),
+                  child: FilterForm(
+                    bloc: _oversightBloc,
+                  ),
+                ),
+              ),
+              bottom: 0,
             ),
           ],
         ),
